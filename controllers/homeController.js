78e1173c17adef3f2402f4eb5112ed1331ad2a56
Krasimir Hristov <102061932+Krasimir-Hristov@users.noreply.github.com>
2022-10-23 20:52:40 +0200

    finish my search requirement from assingment

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -9,7 +9,7 @@ homeController.get('/', async (req, res) => {
 
     if(req.user) {
         view = 'user-home';
-        courses = await getAllByDate();
+        courses = await getAllByDate(req.query.search);
     } else {
         view = 'guest-home';
         courses = await getRecent();
@@ -19,7 +19,8 @@ homeController.get('/', async (req, res) => {
 
     res.render(view, {
         title: 'Home Page',
-        courses
+        courses,
+        search: req.query.search
     });
 });
 
