ebf0efeb794983c8c8fb2ce72ab50c4d0d25a4b1
Krasimir Hristov <102061932+Krasimir-Hristov@users.noreply.github.com>
2022-10-23 21:19:45 +0200

    Create preload.js

---
+++ middlewares/preload.js
@@ -0,0 +1,10 @@
+// const { getById } = require("../services/courseService");
+
+
+
+// module.exports = () => async (req, res, next) => {
+//     const course = await getById(req.params.id);
+//     res.locals.course = course;
+
+//     next();
+// };
